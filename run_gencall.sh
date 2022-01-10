d44efb9ac674eb197cc099d21e8d1b1b7e7472fb
Calvin Leather <cleather@embarkvet.com>
2022-01-10 18:02:49 -0500

    working off manifest

--- run_gencall.sh
+++ run_gencall.sh
@@ -2,14 +2,14 @@
 aws s3 cp s3://genotype-datasets/marker_files/v6_chip_production_probe_lists/Embark_2021_260k_20063270_A1_20211122.egt .
 aws s3 cp s3://genotype-datasets/marker_files/v6_chip_production_probe_lists/Embark_2021_260k_20063270_A1.bpm .
 aws s3 cp s3://genotype-datasets/marker_files/v6_chip_production_probe_lists/Embark_2021_260k_20063270_A1.csv .
-aws s3 sync s3://illumina-embark-sample-level-deliveries/2021-12-14/31001805234805/IDATs/205881370069/ idats
+aws s3 sync s3://illumina-embark-sample-level-deliveries/2022-01-10/31211050209140/IDATs/205885920066/ idats
 
 # Copy the ILMN data to the test bucket for safekeeping
-aws s3 sync s3://illumina-embark-sample-level-deliveries/2021-12-14/31001805234805 s3://scratch-embark/iaap-test/
+aws s3 sync s3://illumina-embark-sample-level-deliveries/2022-01-10/31211050209140 s3://scratch-embark/iaap-test/
 
 # Download an example idat from a new dog
 iaap-cli gencall Embark_2021_260k_20063270_A1.bpm Embark_2021_260k_20063270_A1_20211122.egt outputs -f idats -g
-aws s3 sync outputs s3://scratch-embark/iaap-test/
+aws s3 sync outputs s3://scratch-embark/iaap-test/31211050209140/
 
 # Grab canfam 4 as reference data
 aws s3 cp s3://embark-data-drops/canFam3/canFam3.1_and_SRY.fa .
@@ -19,11 +19,36 @@ aws s3 cp s3://embark-data-drops/canFam3/canFam3.1_and_SRY.fa.bwt .
 aws s3 cp s3://embark-data-drops/canFam3/canFam3.1_and_SRY.fa.dict .
 aws s3 cp s3://embark-data-drops/canFam3/canFam3.1_and_SRY.fa.sa .
 
+# Rename chrX to X, chr10 to 10, Dog_Y to Y etc., which is what ILMN expects based on hg19
+cat canFam3.1_and_SRY.fa | sed -r 's/(chr|Dog_)([0-9YXM]+)/\2/g' > canFam3.1_and_SRY_cleaned.fa
+cat canFam3.1_and_SRY.fa.fai | sed -r 's/(chr|Dog_)([0-9YXM]+)/\2/g' > canFam3.1_and_SRY_cleaned.fa.fai
 
-# Rename chrX to X, which is what ILMN expects based on hg19
-cat canFam3.1_and_SRY.fa | sed -r 's/chr([0-9YXM]*)/\1/g' > canFam3.1_and_SRY.fa
-cat canFam3.1_and_SRY.fa.fai | sed -r 's/chr([0-9YXM]*)/\1/g' > canFam3.1_and_SRY.fa.fai
+# Extract strand information
+pip3 install pandas
+mkdir pandas_tweaked
+# class SourceStrand:
+#     Unknown = 0 = U
+#     Forward = 1  = +
+#     Reverse = 2  = -
 
+python3 -c """
+import pandas as pd
+df=pd.read_csv('Embark_2021_260k_20063270_A1.csv', skiprows=7)
+df[['ILMNStrand_from_id', 'refstrand']] = df['IlmnID'].str.extract('_([BTPM])_([FR])')
+df['MapInfo'] = df['MapInfo'].fillna(0).astype(int)
+df['refstrand'] = df['refstrand'].map({'F': '+', 'R': '-'}).fillna('U')
+df.to_csv('pandas_tweaked/Embark_2021_260k_20063270_A1.csv', index=False)
+"""
+python2 GTCtoVCF/gtc_to_vcf.py \
+    --gtc-paths /outputs/205885920066_R05C01.gtc \
+    --manifest-file /pandas_tweaked/Embark_2021_260k_20063270_A1.csv \
+    --genome-fasta-file /canFam3.1_and_SRY_cleaned.fa \
+    --log-file gtc_to_vcf.log \
+    --include-attributes {GT,GQ,BAF,LRR}
+
+
+# Everything from here down is only needed if you want to use BWA intead of the manifest for alignment
+# information. You should probably do this to double check ILMN's alignment information/refstrand
 
 # Cut header and footer
 tail -n +9 Embark_2021_260k_20063270_A1.csv > Embark_2021_260k_20063270_A1_no_header.csv
@@ -72,7 +97,7 @@ sed 's/TopGenomicSeq,+/TopGenomicSeq,refstrand/' /Embark_2021_260k_20063270_A1_t
 python2 GTCtoVCF/gtc_to_vcf.py \
     --gtc-paths /outputs/205881370069_R11C02.gtc \
     --manifest-file /Embark_2021_260k_20063270_A1_tweaked2.csv \
-    --genome-fasta-file /canFam3.1_and_SRY.fa \
+    --genome-fasta-file /canFam3.1_and_SRY_cleaned.fa \
     --skip-indels # Skip indels required when using bpm manifest. Need extended csv manifest.
     
 aws s3 sync output.vcf s3://scratch-embark/iaap-test/
